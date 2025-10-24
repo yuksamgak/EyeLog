f5eb9165b10a736d205e09ba1f3d5f7c0e29c15d
yuksamgak <80636859+yuksamgak@users.noreply.github.com>
2025-10-25 04:22:20 +0900

    Initial Commit

---
+++ EyeLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EyeLog
+//
+//  Created by 강훈 on 10/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EyeLog/EyeLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeLogApp.swift
+//  EyeLog
+//
+//  Created by 강훈 on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct EyeLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
